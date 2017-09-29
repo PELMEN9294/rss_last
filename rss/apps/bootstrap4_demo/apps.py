9a13ff7a88866670ecc7493ab05a838a26408253
Alex <aleks.lecha@gmail.com>
2017-09-29 17:31:20 +0300

    1

---
+++ rss/apps/bootstrap4_demo/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Bootstrap4DemoConfig(AppConfig):
+    name = 'bootstrap4_demo'
--- rss/settings.py
+++ rss/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap4',
+    'rss.apps.bootstrap4_demo',
     'rss.apps.rss_news',
 ]
 
