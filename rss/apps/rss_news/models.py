e95a6125b91d3d8610f59b9b87c799f0b1b21450
Alex <aleks.lecha@gmail.com>
2017-10-18 17:50:42 +0300

    1

--- rss/apps/rss_news/models.py
+++ rss/apps/rss_news/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-
+import datetime
 # Create your models here.
 
 class News(models.Model):
     id = models.BigIntegerField(primary_key=True)
     title = models.TextField(max_length=50)
     text = models.TextField()
+    date = models.DateField(default=str(datetime.datetime.now())[:10])
 
     def __str__(self):
         return str(self.id) + "_" + str(self.title)
